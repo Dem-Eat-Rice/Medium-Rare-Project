86055af5b3f7a7ff94e161dbf85659d150a7bdc1
Mustafa <mustafaomousa@icloud.com>
2020-12-01 14:38:40 -0600

    created the posts model

---
+++ db/models/post.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Post = sequelize.define('Post', {
+    authorId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    title: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    body: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    }
+  }, {});
+  Post.associate = function (models) {
+    Post.belongsTo(models.User, { foreignKey: "authorId" });
+  };
+  return Post;
+};
