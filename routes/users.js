f334c4df5fa72cce1d1e6fcdd208553a5452851b
Mustafa <mustafaomousa@icloud.com>
2020-12-02 10:47:36 -0600

    fixed error handling

--- routes/users.js
+++ routes/users.js
@@ -6,24 +6,29 @@ const db = require('../db/models');
 const validateSignUpForm = [
   check("username")
     .exists({ checkFalsy: true })
-    .withMessage("Please provide a username")
+    .withMessage("Please provide a username"),
+  check("username")
     .isLength({ max: 15 })
     .withMessage("Username cannot be longer than 15 characters"),
   check("firstName")
     .exists({ checkFalsy: true })
-    .withMessage("Please provide a first name")
+    .withMessage("Please provide a first name"),
+  check("firstName")
     .isLength({ max: 30 })
     .withMessage("First name cannot be longer than 30 characters"),
   check("lastName")
     .exists({ checkFalsy: true })
-    .withMessage("Please provide a last name")
+    .withMessage("Please provide a last name"),
+  check("lastName")
     .isLength({ max: 30 })
     .withMessage("Last name cannot be longer than 30 characters"),
   check("email")
     .exists({ checkFalsy: true })
-    .withMessage("Please provide an email")
+    .withMessage("Please provide an email"),
+  check("email")
     .isEmail()
-    .withMessage("Please provide a valid email")
+    .withMessage("Please provide a valid email"),
+  check("email")
     .custom((value) => {
       return db.User.findOne({
         where: {
@@ -40,21 +45,22 @@ const validateSignUpForm = [
   check("confirmPassword")
     .exists({ checkFalsy: true })
     .withMessage("Please provide a password")
-    .custom((value, req) => {
+  // .custom((value, req) => {
 
-    })
+  // })
 
 
 ]
 
-router.get("/", csrfProtection, asyncHandler(async (req, res) => {
+router.get("/sign-up", csrfProtection, asyncHandler(async (req, res) => {
   res.render("sign-up", { csrfToken: req.csrfToken() });
 }));
 
-router.post("/", validateSignUpForm, csrfProtection, asyncHandler(async (req, res) => {
+router.post("/sign-up", validateSignUpForm, csrfProtection, asyncHandler(async (req, res) => {
   const { username, firstName, lastName, email, password } = req.body;
+  console.log(username);
   const validationErrors = validationResult(req);
-  if (validationErrors) {
+  if (!validationErrors.isEmpty()) {
     const errors = validationErrors.array().map((error) => error.msg);
     const err = Error("Bad Request");
     err.status = 400;
@@ -63,8 +69,8 @@ router.post("/", validateSignUpForm, csrfProtection, asyncHandler(async (req, re
     return res.status(400).render('sign-up');
   }
 
-  res.redirect('/users');
-
+  // res.redirect('/users/sign-up');
+  res.redirect("/");
 }));
 
 module.exports = router;
