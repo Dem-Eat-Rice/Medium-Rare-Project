0f1e7ab2f917c99cecf057081aef7cd95fb340f9
jimmy <demeatricej.sherrod@gmail.com>
2020-12-01 18:24:46 -0600

    ACTUAL error handling code added

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,71 @@
 const express = require("express");
 const router = express.Router();
-const { asyncHandler, csrf, csrfProtection } = require('../utils');
+const { asyncHandler, csrf, csrfProtection, validationResult, check } = require('../utils');
+const db = require('../db/models');
+const { noExtendLeft } = require("sequelize/types/lib/operators");
 
+const validateSignUpForm = [ 
+  check("username")
+    .exists({ checkFalsy: true})
+    .withMessage("Please provide a username")
+    .isLength({max: 15})
+    .withMessage("Username cannot be longer than 15 characters"),
+  check("firstName")
+    .exists({ checkFalsy: true})
+    .withMessage("Please provide a first name")
+    .isLength({max: 30})
+    .withMessage("First name cannot be longer than 30 characters"),
+  check("lastName")
+    .exists({ checkFalsy: true})
+    .withMessage("Please provide a last name")
+    .isLength({max: 30})
+    .withMessage("Last name cannot be longer than 30 characters"),
+  check("email")
+    .exists({ checkFalsy: true})
+    .withMessage("Please provide an email")
+    .isEmail()
+    .withMessage("Please provide a valid email")
+    .custom((value) => {
+      return db.User.findOne({
+        where: {
+          email: value
+        }
+      })
+      .then((user) => {
+        return Promise.reject('The provided email is already in use by another account')
+      })
+    }),
+  check("password")
+    .exists({ checkFalsy: true})
+    .withMessage("Please provide a password"),
+  check("confirmPassword")
+    .exists({ checkFalsy: true})
+    .withMessage("Please provide a password")
+    .custom((value, req)  => {
+
+    })
+
+  
+]
 
 router.get("/", csrfProtection, asyncHandler(async (req, res) => {
   res.render("sign-up", { csrfToken: req.csrfToken() });
 }));
 
-router.post("/", csrfProtection, asyncHandler(async (req, res) => {
-  const { } = req.body;
+router.post("/", validateSignUpForm, csrfProtection, asyncHandler(async (req, res) => {
+  const { username, firstName, lastName, email, password } = req.body;
+  const validationErrors = validationResult(req);
+  if (validationErrors) {
+    const errors = validationErrors.array().map((error) => error.msg);
+    const err = Error("Bad Request");
+    err.status = 400;
+    err.title = "Bad Request";
+    err.errors = errors;
+    return res.status(400).render('sign-up');
+  }
+
+  res.redirect('/users');
+
 }));
 
 module.exports = router;
--- utils.js
+++ utils.js
@@ -1,7 +1,20 @@
 const csrf = require("csurf");
+const {validationResult, check } = require("express-validator");
 
 const csrfProtection = csrf({ cookie: true });
 
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
-module.exports = { asyncHandler, csrf, csrfProtection };
+// const handleValidationErrors = (req, res, next) => {
+//     const validationErrors = validationResult(req);
+//     if(!validationErrors.isEmpty()) {
+//         const errors = validationErrors.array().map((error) => error.msg);
+//         const err = Error("Bad Request");
+//         err.status = 400;
+//         err.title = "Bad Request";
+//         err.errors = errors;
+//         return next(err);
+//     }
+//     next();
+// }
+module.exports = { asyncHandler, csrf, csrfProtection, validationResult, check };
