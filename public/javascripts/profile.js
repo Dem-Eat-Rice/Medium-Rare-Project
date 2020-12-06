fb91517c7c94a686e6259beb8737af1dc8211d33
Mustafa <mustafaomousa@icloud.com>
2020-12-06 14:04:05 -0600

    finishing change password

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,10 +1,43 @@
+// const e = require("express");
+
 document.addEventListener("DOMContentLoaded", async () => {
   const editButton = document.getElementById("profile-button-edit");
   const deleteButton = document.getElementById("profile-button-delete");
+  const changePassword = document.getElementById("profile-button-changepassword");
 
   editButton.addEventListener("click", (event) => {
+    event.preventDefault();
     const editUserInfo = document
       .querySelectorAll(".update-user")
       .forEach((input) => input.setAttribute("style", ""));
   });
+
+
+  const passwordLabel = document.createElement("label")
+  passwordLabel.innerHTML = "Enter a new password: ";
+  const confirmPasswordLabel = document.createElement("label")
+  confirmPasswordLabel.innerHTML = "Confirm your new password: ";
+
+  const form = document.createElement("form");
+  form.setAttribute("action", "/users/update-password");
+  form.setAttribute("method", "post");
+
+  const password = document.createElement("input");
+  password.setAttribute("type", "text");
+  password.setAttribute("name", "password");
+
+  const confirmPassword = document.createElement("input");
+  confirmPassword.setAttribute("type", "text");
+  confirmPassword.setAttribute("name", "confirmPassword");
+
+  changePassword.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    form.appendChild(passwordLabel);
+    form.appendChild(password);
+    form.appendChild(document.createElement("br"))
+    form.appendChild(confirmPasswordLabel)
+    form.appendChild(confirmPassword);
+    document.getElementById("changePassword").appendChild(form)
+  })
 });
